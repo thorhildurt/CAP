52cd1dc662f4fc64a8ad8029c7854f4221113bd8
Thorhildur Thorleiksdottir <thorhildurt15@ru.is>
2020-11-23 13:45:36 +0100

    updattee userr and pw in mvc

--- CAwebapp/Controllers/HomeController.cs
+++ CAwebapp/Controllers/HomeController.cs
@@ -108,9 +108,13 @@ namespace CAwebapp.Controllers
             user.UserId = userId;
             Console.WriteLine(user.FirstName);
             StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json"); 
-            string endpoint = "/auth/login";  
+            string endpoint = "/user";  
+            var response = _httpClient.PutAsync(endpoint, content).Result;
 
-
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)  
+            {  
+                Console.WriteLine("Updating user successfull");
+            }
             TempData["Profile"] = JsonConvert.SerializeObject(user); 
             return RedirectToAction("Profile");
         }
